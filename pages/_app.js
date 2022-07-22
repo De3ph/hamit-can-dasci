af42476eb9d630b44dca42cb630591aedb3c0a2c
De3ph <05190000108@ogrenci.ege.edu.tr>
2022-07-22 17:08:26 +0300

    layout folder moved into components folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "@styles/globals.css";
-import Layout from "../layout/Layout";
+import Layout from "../components/layout/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function Application({ Component, pageProps }) {
