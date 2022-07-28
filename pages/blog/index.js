fd68ea67818e52f234fa672bed94d553b0cb4098
De3ph <05190000108@ogrenci.ege.edu.tr>
2022-07-28 22:57:19 +0300

    blog page fun video

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,43 @@
 import React from "react";
 import Blog from "@components/Blog/Blog";
-const blog = () => {
+// import { getClient } from "@lib/sanity/sanity";
+// import { groq } from "next-sanity";
+
+// const query = groq`
+// *[_type == "post"] {
+//   title,
+//   mainImage,
+//   author -> {
+//     name,
+//     image
+//   }
+// }
+// `;
+
+// export const getStaticProps = async ({ preview = false }) => {
+//   const posts = await getClient(preview).fetch(query);
+//   return {
+//     props: {
+//       posts,
+//       preview,
+//     },
+//     revalidate: 30,
+//   };
+// };
+
+const index = ({ posts }) => {
   return (
     <>
-      <Blog />
+      {/* <Blog posts={posts} /> */}
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/V5nzZWEtCnE?autoplay=1"
+        title="YouTube video player"
+        allow="autoplay loop"
+      ></iframe>
     </>
   );
 };
 
-export default blog;
+export default index;
