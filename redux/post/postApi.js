64b3be045a70ff0b10ae98a81778738e54680cb3
De3ph <05190000108@ogrenci.ege.edu.tr>
2022-08-25 23:56:54 +0300

    redux kaldırıldı

--- redux/post/postApi.js
+++
@@ -1,8 +0,0 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
-import { sanityBaseQuery } from "./postQueryFn";
-import { getAllPostQuery } from "querys";
-
-export const postApi = createApi({
-  reducerPath: "postApi",
-  baseQuery: sanityBaseQuery({ baseQuery: getAllPostQuery }),
-});
--- redux/post/postStore.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postReducer from "./postSlice";
-
-export const store = configureStore({
-  reducer: {
-    post: postReducer,
-  },
-});
